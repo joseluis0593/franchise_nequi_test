e12d541396961bbf2200b30ddcbe0f6ac642b1bc
joseluis0593 <32516383+joseluis0593@users.noreply.github.com>
2024-10-03 12:29:41 -0500

    initial project

---
+++ applications/app-service/src/main/java/co/com/nequi/colombia/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.nequi.colombia.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.nequi.colombia.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
